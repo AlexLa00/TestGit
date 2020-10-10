6f1371bf96e0f45a57eb0f26d8003d0f20adcaf1
AlexLa00 <alexlfischer.00@gmail.com>
2020-10-10 15:17:50 -0400

    This is the first commit

---
+++ app/src/main/java/alex/example/testgit/MainActivity.java
@@ -0,0 +1,19 @@
+package alex.example.testgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public static void main(){
+        System.out.println("Hello World");
+    }
+}
